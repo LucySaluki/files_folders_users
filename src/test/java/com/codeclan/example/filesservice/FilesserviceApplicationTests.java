776f1f3b7530a45e1f806c3b716593f938e7c939
LucySaluki mslucyarnold@gmail.com <mslucyarnold@gmail.com>
2020-10-06 18:58:54 +0100

    initial commit

---
+++ src/test/java/com/codeclan/example/filesservice/FilesserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.codeclan.example.filesservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class FilesserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
